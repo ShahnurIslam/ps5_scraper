81e7693f26566e03e2cf200563ea6d7a5d374071
shahnur.islam <shahnur.islam@royalmail.com>
2020-10-06 16:37:37 +0100

    Amazon scraper added and working

--- index.ts
+++ index.ts
@@ -1,4 +1,9 @@
 import {Bot} from "./src/Bot"
+import {AmazonUK} from "./src/Crawlers/AmazonUk"
 
 // console.log("hello")
 new Bot().start();
+
+const r = new AmazonUK().getStock();
+
+console.log(r)
--- src/Bot.ts
+++ src/Bot.ts
@@ -1,14 +1,20 @@
 import {Logger,LogLevel} from "./Logger"
+import { AmazonUK } from "./Crawlers/AmazonUk"
 
 export class Bot {
     logger: Logger
+    am: AmazonUK
 
     constructor(){
         this.logger = new Logger(LogLevel.Info)
+        this.am = new AmazonUK
     }
 
-    public start(){
+    public async start(){
         this.logger.info("starting bot scraper")
+        console.log(this.am.getRetailerName())
+        console.log(await this.am.crawlSite('B08H95Y452'))
+        
     }
 
 }
